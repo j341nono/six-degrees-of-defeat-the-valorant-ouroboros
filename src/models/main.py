3e539f7529fb0846b45c9a3d0f1cc6891c5ec2fb
nonomura <kanakana.nono1224@gmail.com>
2025-08-28 01:39:36 +0900

    feat(models): Add team-to-index mapping utility

--- src/models/main.py
+++ src/models/main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import networkx as nx
 
 SAVE_MATCH_DATA="data/processed/match_data.jsonl"
 
@@ -11,7 +12,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def load_json(path: str) -> list:
+def load_json_line(path: str) -> list:
     match_list = []
     with open(path, "r", encoding="utf-8") as f:
         for line in f:
@@ -19,16 +20,61 @@ def load_json(path: str) -> list:
     return match_list
             
 
+def make_team_to_idx_dict(match_data: list) -> tuple[dict[str, int], dict[int, str]]:
+    all_team = set()
+    for one_match in match_data:
+        all_team.add(one_match.get("team1"))
+        all_team.add(one_match.get("team2"))
+    team_to_idx = {}
+    idx_to_team = {}
+    for i, team in enumerate(sorted(list(all_team))):
+        team_to_idx[team] = i
+        idx_to_team[i] = team
+    return team_to_idx, idx_to_team
+
+
+def network():
+    G = nx.DiGrapsh()
+
+
+
 def main():
     args = parse_args()
+    match_data = load_json_line(SAVE_MATCH_DATA)
+    team_to_idx, idx_to_team = make_team_to_idx_dict(match_data)
+    print(len(team_to_idx))
 
 
 
-def debug():
-    match_data = load_json(SAVE_MATCH_DATA)
+def debug_load_json():
+    match_data = load_json_line(SAVE_MATCH_DATA)
     print(match_data[:2])
     print(f"data num: {len(match_data)}")
 
+def debug_make_dict():
+    match_data = load_json_line(SAVE_MATCH_DATA)
+    team_to_id = {}
+    for one_match in match_data:
+        team1 = one_match.get("team1")
+        team2 = one_match.get("team2")
+        print(team1)
+        import sys
+        sys.exit()
+
+def debug_make_team_to_idx():
+    match_data = load_json_line(SAVE_MATCH_DATA)
+    all_team = set()
+    for one_match in match_data:
+        all_team.add(one_match.get("team1"))
+        all_team.add(one_match.get("team2"))
+    team_to_idx = {}
+    idx_to_team = {}
+    for i, team in enumerate(sorted(list(all_team))):
+        team_to_idx[team] = i
+        idx_to_team[i] = team
+    # print(team_to_idx)
+    print(idx_to_team)
+
 
 if __name__ == "__main__":
-    debug()
+    main()
